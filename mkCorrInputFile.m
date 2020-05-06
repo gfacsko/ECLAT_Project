6a358eea47680b9c992ec04fb4af582aea8509af
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2020-05-07 00:18:00 +0200

    06/05/2020 new version

--- mkCorrInputFile.m
+++ mkCorrInputFile.m
@@ -422,6 +422,10 @@ function [ error ] = mkCorrInputFile(tStart,tEnd,isOMNI,isBz,strSuffix)
             end; 
             if (strcmp(strSuffix,'-msh')),
                 axis([t(1) t(numel(t)) -30 20]);
+                set(gca,'XTick',tStart:1./48.:tEnd);
+                set(gca,'XTickLabel',{'02:30','03:00','03:30','04:00',...
+                    '04:30','05:00','05:30','06:00','06:30','07:00',...
+                    '07:30','08:00','08:30','09:00'}); 
             end;             
             if (strcmp(strSuffix,'-ns')),
                 axis([t(1) t(numel(t)) -10 10]);
